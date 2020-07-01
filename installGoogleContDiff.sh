e7824d471f89c9307b2062121f232b0b35440180
kants3 <shashi.kant@in.pega.com>
2020-07-01 15:35:18 +0530

    tst

--- installGoogleContDiff.sh
+++ installGoogleContDiff.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-USERNAME=$1
-PASSWORD=$2
+img1=$1
+img2=$2
+cul -LO https://storage.googleapis.com/container-diff/latest/container-diff-linux-amd64 && chmod +x container-diff-linux-amd64 && mkdir -p $HOME/bin && export PATH=$PATH:$HOME/bin && mv container-diff-linux-amd64 $HOME/bin/container-diff
 
-curl -LO https://storage.googleapis.com/container-diff/latest/container-diff-linux-amd64 && chmod +x container-diff-linux-amd64 && mkdir -p $HOME/bin && export PATH=$PATH:$HOME/bin && mv container-diff-linux-amd64 $HOME/bin/container-diff
+#container-diff analyze daemon://nginx --type=history --type=size --type=rpm  --type=pip --type=apt --type=node
 
-container-diff analyze daemon://nginx --type=history --type=size --type=rpm  --type=pip --type=apt --type=node
+container-diff diff daemon://$1 daemon://$2 --type=history --type=apt --type=node
