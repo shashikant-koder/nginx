a7692119423c098357b6c0765ca043c5060748cc
kants3 <shashi.kant@in.pega.com>
2020-07-01 15:10:16 +0530

    docker diff

---
+++ installGoogleContDiff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+USERNAME=$1
+PASSWORD=$2
+
+curl -LO https://storage.googleapis.com/container-diff/latest/container-diff-linux-amd64 && chmod +x container-diff-linux-amd64 && mkdir -p $HOME/bin && export PATH=$PATH:$HOME/bin && mv container-diff-linux-amd64 $HOME/bin/container-diff
+
+container-diff analyze daemon://$DOCKER_USERNAME/nginx:latest --type=history --type=size --type=rpm  --type=pip --type=apt --type=node
